483fec5b88c78fb9cf090f64482cab15d9175ba5
cjq <691424873@qq.com>
2018-09-04 18:31:32 +0800

    ui

--- app/src/main/java/com/app/qingyi/activitys/CreateActivity.java
+++ app/src/main/java/com/app/qingyi/activitys/CreateActivity.java
@@ -1,17 +1,28 @@
 package com.app.qingyi.activitys;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.app.qingyi.R;
 
-public class CreateActivity extends BaseActivity implements View.OnClickListener {
+import java.util.ArrayList;
 
+import es.dmoral.toasty.Toasty;
+import me.iwf.photopicker.PhotoPicker;
+
+public class CreateActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        initView();
+    }
+
+    private void initView() {
+        ((TextView) findViewById(R.id.tv_tit)).setText("发布");
     }
 
     @Override
@@ -20,6 +31,27 @@ public class CreateActivity extends BaseActivity implements View.OnClickListener
             case R.id.iv_close:
                 finish();
                 break;
+            case R.id.imgSelect:
+                PhotoPicker.builder()
+                        .setPhotoCount(1)
+                        .setShowCamera(true)
+                        .setShowGif(true)
+                        .setPreviewEnabled(false)
+                        .start(this, PhotoPicker.REQUEST_CODE);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == PhotoPicker.REQUEST_CODE) {
+            if (data != null) {
+                ArrayList<String> photos =
+                        data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS);
+                Toasty.normal(this,photos.get(0)).show();
+            }
         }
     }
 }
