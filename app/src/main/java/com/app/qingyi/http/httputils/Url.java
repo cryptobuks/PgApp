ae7c34392a87bff1935b9fb7f23c4809ee504036
cjq <691424873@qq.com>
2018-09-12 19:10:42 +0800

    70

--- app/src/main/java/com/app/qingyi/http/httputils/Url.java
+++ app/src/main/java/com/app/qingyi/http/httputils/Url.java
@@ -1,10 +1,10 @@
 package com.app.qingyi.http.httputils;
 
 public class Url {
+//    public static String HTTP = "http://";
+//    public static String IP = "192.168.1.168:8100";
     public static String HTTP = "http://";
-    public static String IP = "192.168.1.168:8100";
-//    public static String HTTP = "https://";
-//    public static String IP = "mobipromo.io";
+    public static String IP = "topniu.club";
     public static String BASECODE = "/qingyi";
     public static String BASE_URL = HTTP + IP + BASECODE;
 }
